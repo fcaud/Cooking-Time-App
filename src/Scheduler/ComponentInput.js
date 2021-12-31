dd48237d400dc11323153827e8feb310eee3cd91
fcaud <78268012+fcaud@users.noreply.github.com>
2021-12-31 16:58:39 +0000

    input of more wire frame

--- src/Scheduler/ComponentInput.js
+++ src/Scheduler/ComponentInput.js
@@ -10,6 +10,79 @@ export default function ComponentInput() {
           <input type="submit" value="Add component"></input>
         </form>
       </div>
+      <div className="component-time-inputs">
+        <div className="row">
+          <div className="col">
+            <h3>Prep Time</h3>
+          </div>
+          <div className="col">
+            <h3>Cook Time</h3>
+          </div>
+          <div className="col">
+            <h3>Post Cook Time</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <button>Delete</button>
+          </div>
+          <div className="col">
+            <p>Chicken</p>
+          </div>
+          <div className="col">
+            <input type="time"></input>
+          </div>
+          <div className="col">
+            <input type="time"></input>
+          </div>
+          <div className="col">
+            <input type="time"></input>
+          </div>
+          <div className="col">
+            <button>note</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <button>Delete</button>
+          </div>
+          <div className="col">
+            <p>Roast Potatoes</p>
+          </div>
+          <div className="col">
+            <input type="time"></input>
+          </div>
+          <div className="col">
+            <input type="time"></input>
+          </div>
+          <div className="col">
+            <input type="time"></input>
+          </div>
+          <div className="col">
+            <button>note</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <button>Delete</button>
+          </div>
+          <div className="col">
+            <p>Parsnips</p>
+          </div>
+          <div className="col">
+            <input type="time"></input>
+          </div>
+          <div className="col">
+            <input type="time"></input>
+          </div>
+          <div className="col">
+            <input type="time"></input>
+          </div>
+          <div className="col">
+            <button>note</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
