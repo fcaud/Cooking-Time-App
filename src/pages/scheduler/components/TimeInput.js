2178cc1d3321f1406408ee4e8f1c57e510ac721e
fcaud <78268012+fcaud@users.noreply.github.com>
2022-01-13 15:53:40 +0000

    TimeInput onchange additions

--- src/pages/scheduler/components/TimeInput.js
+++ src/pages/scheduler/components/TimeInput.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TimeInput.css";
 
 export default function TimeInput() {
+  const [time, setTime] = useState("");
+  const [startOrEnd, setStartOrEnd] = useState("");
+
   return (
     <div className="TimeInput">
       <form>
-        <select name="time" id="time-select">
+        <select
+          name="time"
+          id="time-select"
+          value={startOrEnd}
+          onchange={(event) => setStartOrEnd(event.target.value)}
+        >
           <option value="">--Select--</option>
           <option value="start">Start time</option>
           <option value="end">End time</option>
         </select>
-        <input type="time"></input>
+        <input
+          type="time"
+          value={time}
+          onchange={(event) => setTime(event.target.value)}
+        ></input>
       </form>
     </div>
   );
